0bca5c18384277f2af30366ddd76fab7b902af04
hooke <cookmycode@gmail.com>
2024-12-21 23:33:42 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ColorCard',
+  webDir: 'dist'
+};
+
+export default config;
